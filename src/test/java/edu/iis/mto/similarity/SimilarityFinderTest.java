d618a1590a4de98193905bc507969d836a2f29d3
Jakub Wlaźlak <kubikapb@gmail.com>
2022-03-21 18:22:26 +0100

    Testy stanu: added test oneSequenceIsEmpty

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -18,4 +18,38 @@ class SimilarityFinderTest {
         double result = similarityFinder.calculateJackardSimilarity(sequence,secondSequence);
         assertEquals(1,result);
     }
+
+    @Test
+    void oneSequenceIsEmpty(){
+        int[] sequence = {1, 21, 32, 46, 58};
+        int[] secondSequence = {};
+        SimilarityFinder similarityFinder = new SimilarityFinder((elem, sequence1) ->
+        {
+            SearchResult searchResult = null;
+            if(elem==1)
+            {
+                searchResult = SearchResult.builder().withPosition(0).withFound(false).build();
+            }
+            if (elem==21)
+            {
+                searchResult = SearchResult.builder().withPosition(1).withFound(false).build();
+            }
+            if (elem==32)
+            {
+                searchResult = SearchResult.builder().withPosition(2).withFound(false).build();
+            }
+            if (elem==46)
+            {
+                searchResult = SearchResult.builder().withPosition(3).withFound(false).build();
+            }
+            if (elem==58)
+            {
+                searchResult = SearchResult.builder().withPosition(4).withFound(false).build();
+            }
+            return searchResult;
+        });
+        double result = similarityFinder.calculateJackardSimilarity(sequence, secondSequence);
+        assertEquals(0,result);
+    }
+
 }
