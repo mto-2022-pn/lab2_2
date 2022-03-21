cbb0ddc16470fb80f4dc01b2b7c9a38de03eb5fc
Jakub Wlaźlak <kubikapb@gmail.com>
2022-03-21 18:18:53 +0100

    Testy stanu: added test bothSequencesAreEmpty

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -1,14 +1,21 @@
 package edu.iis.mto.similarity;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
+import edu.iis.mto.searcher.SearchResult;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class SimilarityFinderTest {
 
     @Test
-    void test() {
-        fail("Not yet implemented");
-    }
+    void bothSequencesAreEmpty() {
+        int[] sequence = {};
+        int[] secondSequence = {};
+        SimilarityFinder similarityFinder = new SimilarityFinder((elem, sequence1) -> SearchResult.builder().build());
 
+        double result = similarityFinder.calculateJackardSimilarity(sequence,secondSequence);
+        assertEquals(1,result);
+    }
 }
