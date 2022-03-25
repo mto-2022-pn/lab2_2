6674eb24b6035c7f6dbcce8e8e801dad9e4eea62
Konrad Basta <228685@edu.p.lodz.pl>
2022-03-26 00:42:00 +0100

    FirstSeqEmpty

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -1,15 +1,19 @@
 package edu.iis.mto.similarity;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
 
 import edu.iis.mto.searcher.SearchResult;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import java.util.Random;
 
 class SimilarityFinderTest {
 
+    private static final double DIFFERENT = 0.0d;
+    private static final double IDENTITY = 1.0d;
+
     private SimilarityFinder similarityFinder;
+    private Random random = new Random();
 
     @BeforeEach
     void SetUpTest() {
@@ -21,7 +25,16 @@ class SimilarityFinderTest {
     void BothSeqEmpty() {
         int[] seq1 = {};
         int[] seq2 = {};
-        double expected = 1.0d;
+        double expected = IDENTITY;
+        double result = similarityFinder.calculateJackardSimilarity(seq1, seq2);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void FirstSeqEmpty() {
+        int[] seq1 = {};
+        int[] seq2 = random.ints(random.nextInt(9999) + 1).toArray();
+        double expected = DIFFERENT;
         double result = similarityFinder.calculateJackardSimilarity(seq1, seq2);
         assertEquals(expected, result);
     }
