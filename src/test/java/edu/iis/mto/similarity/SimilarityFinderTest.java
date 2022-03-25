f8dd12356cb0841de4489f2cbbe3d6e9e355d7a4
Konrad Basta <228685@edu.p.lodz.pl>
2022-03-26 00:35:52 +0100

    BothSeqEmpty

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -1,14 +1,28 @@
 package edu.iis.mto.similarity;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
+import edu.iis.mto.searcher.SearchResult;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class SimilarityFinderTest {
 
-    @Test
-    void test() {
-        fail("Not yet implemented");
+    private SimilarityFinder similarityFinder;
+
+    @BeforeEach
+    void SetUpTest() {
+        similarityFinder = new SimilarityFinder(
+                (element, sequence) -> SearchResult.builder().build());
     }
 
+    @Test
+    void BothSeqEmpty() {
+        int[] seq1 = {};
+        int[] seq2 = {};
+        double expected = 1.0d;
+        double result = similarityFinder.calculateJackardSimilarity(seq1, seq2);
+        assertEquals(expected, result);
+    }
 }
