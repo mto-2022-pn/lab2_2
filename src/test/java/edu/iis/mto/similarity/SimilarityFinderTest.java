cf7aa024781005f56e96d89a98e1b2c14d975c8d
Bartosz <228805@edu.p.lodz.pl>
2022-03-25 14:00:03 +0100

    Condition tests: created TwoSequencesAreEmpty test and initSimilarityFinder

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -2,13 +2,24 @@ package edu.iis.mto.similarity;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import edu.iis.mto.searcher.SearchResult;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class SimilarityFinderTest {
 
+    SimilarityFinder similarityFinder;
+    @BeforeEach
+    void initSimilarityFinder(){
+        similarityFinder = new SimilarityFinder(((elem, sequence) -> SearchResult.builder().build()));
+    }
     @Test
-    void test() {
-        fail("Not yet implemented");
+    void TwoSequencesAreEmpty() {
+        int[] seq1={};
+        int[] seq2={};
+        double expected=1;
+        double result=similarityFinder.calculateJackardSimilarity(seq1,seq2);
+        assertEquals(expected,result);
     }
 
 }
