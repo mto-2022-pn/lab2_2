c4e8d72068285c3259eb2f65e052f9fd1d207975
Adrian Pietrzak <78472697+AdrianPietrz@users.noreply.github.com>
2022-03-25 12:24:33 +0100

    test stanu: test pustych tablic

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -1,14 +1,21 @@
 package edu.iis.mto.similarity;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
+import edu.iis.mto.searcher.SearchResult;
+import edu.iis.mto.searcher.SequenceSearcher;
 import org.junit.jupiter.api.Test;
 
 class SimilarityFinderTest {
 
     @Test
-    void test() {
-        fail("Not yet implemented");
+    void SentencesAreEmpty() {
+        int seq [] = new int[0];
+        int seq1 [] = new int[0];
+        SimilarityFinder similarityFinder = new SimilarityFinder((elem, sequence) -> SearchResult.builder().build());
+        double result = similarityFinder.calculateJackardSimilarity(seq,seq1);
+        assertEquals(1,result);
     }
 
 }
