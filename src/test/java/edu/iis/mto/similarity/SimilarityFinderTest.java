650e52b712f3e18040eeef2babd27c1d6d0dfdc5
Konrad Basta <228685@edu.p.lodz.pl>
2022-03-26 01:10:18 +0100

    NotHaveCommonElements

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -5,7 +5,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import edu.iis.mto.searcher.SearchResult;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 class SimilarityFinderTest {
 
@@ -44,6 +49,27 @@ class SimilarityFinderTest {
         int[] seq1 = random.ints(random.nextInt(9999) + 1).toArray();
         int[] seq2 = {};
         double expected = DIFFERENT;
+        double result = similarityFinder.calculateJackardSimilarity(seq1, seq2);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void NotHaveCommonElements() {
+        int[] seq1 = {1,5,3};
+        int[] seq2 = {6,8,4,2,9,7};
+        double expected = DIFFERENT;
+        SimilarityFinder similarityFinder = new SimilarityFinder(((elem, sequence) -> {
+            switch (elem) {
+                case 1:
+                    return SearchResult.builder().withPosition(0).withFound(false).build();
+                case 5:
+                    return SearchResult.builder().withPosition(1).withFound(false).build();
+                case 3:
+                    return SearchResult.builder().withPosition(2).withFound(false).build();
+                default:
+                    return null;
+            }
+        }));
         double result = similarityFinder.calculateJackardSimilarity(seq1, seq2);
         assertEquals(expected, result);
     }
