39375df5699cca66a42ad103585b2fa763d541e5
Wojciech Farej <228730@edu.p.lodz.pl>
2022-03-26 11:34:03 +0100

    Both sequences empty

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -1,14 +1,23 @@
 package edu.iis.mto.similarity;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
+import edu.iis.mto.searcher.SearchResult;
+
 class SimilarityFinderTest {
 
+    private static double IDENTICAL = 1.0d;
+    private static double TOTTALLY_DIFFRENT = 0.0d;
+
     @Test
-    void test() {
-        fail("Not yet implemented");
+    void testEmptyArrays() {
+        SimilarityFinder finder = new SimilarityFinder((element, sequence) -> SearchResult.builder().build());
+        int seq1[] = {};
+        int seq2[] = {};
+        double res = finder.calculateJackardSimilarity(seq1, seq2);
+        assertEquals(IDENTICAL, res);
     }
 
 }
