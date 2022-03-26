4c8488fdfc7c47f3e0168d4a64576ec38fab003c
Wojciech Farej <228730@edu.p.lodz.pl>
2022-03-26 11:36:28 +0100

    test when one of arrays is empty

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -20,4 +20,22 @@ class SimilarityFinderTest {
         assertEquals(IDENTICAL, res);
     }
 
+    @Test
+    void testFirstArrayAsEmpty() {
+        SimilarityFinder finder = new SimilarityFinder((element, sequence) -> SearchResult.builder().build());
+        int seq1[] = {};
+        int seq2[] = {3,5,7,9};
+        double res = finder.calculateJackardSimilarity(seq1, seq2);
+        assertEquals(TOTTALLY_DIFFRENT, res);
+    }
+
+    @Test
+    void testSecondArrayAsEmpty() {
+        SimilarityFinder finder = new SimilarityFinder((element, sequence) -> SearchResult.builder().build());
+        int seq1[] = {1,6,9,15};
+        int seq2[] = {};
+        double res = finder.calculateJackardSimilarity(seq1, seq2);
+        assertEquals(TOTTALLY_DIFFRENT, res);
+    }
+
 }
