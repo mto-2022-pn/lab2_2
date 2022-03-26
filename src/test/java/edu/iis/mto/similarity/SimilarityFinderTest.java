259646abbe0d1df71dba7aa296d09d72ce079e0b
Wojciech Farej <228730@edu.p.lodz.pl>
2022-03-26 12:17:18 +0100

    added testArraysHaveSomeEleementInCommon

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -24,7 +24,7 @@ class SimilarityFinderTest {
     void testFirstArrayAsEmpty() {
         SimilarityFinder finder = new SimilarityFinder((element, sequence) -> SearchResult.builder().build());
         int seq1[] = {};
-        int seq2[] = {3,5,7,9};
+        int seq2[] = { 3, 5, 7, 9 };
         double res = finder.calculateJackardSimilarity(seq1, seq2);
         assertEquals(TOTTALLY_DIFFRENT, res);
     }
@@ -32,7 +32,7 @@ class SimilarityFinderTest {
     @Test
     void testSecondArrayAsEmpty() {
         SimilarityFinder finder = new SimilarityFinder((element, sequence) -> SearchResult.builder().build());
-        int seq1[] = {1,6,9,15};
+        int seq1[] = { 1, 6, 9, 15 };
         int seq2[] = {};
         double res = finder.calculateJackardSimilarity(seq1, seq2);
         assertEquals(TOTTALLY_DIFFRENT, res);
@@ -41,10 +41,34 @@ class SimilarityFinderTest {
     @Test
     void testArraysDontHaveCommonElements() {
         SimilarityFinder finder = new SimilarityFinder((element, sequence) -> SearchResult.builder().build());
-        int seq1[] = {0,5,11,15};
-        int seq2[] = {2,3,7,99};
+        int seq1[] = { 0, 5, 11, 15 };
+        int seq2[] = { 2, 3, 7, 99 };
         double res = finder.calculateJackardSimilarity(seq1, seq2);
         assertEquals(TOTTALLY_DIFFRENT, res);
+    }
+
+    @Test
+    void testArraysHaveSomeEleementInCommon() {
+        SimilarityFinder finder = new SimilarityFinder((element, sequence) -> {
+            SearchResult result = SearchResult.builder().build(); // default: not found (position doesn't really matter
+                                                                  // to test Similarity)
+            switch (element) {
+                case -15:
+                    result = SearchResult.builder().withPosition(0).withFound(true).build();
+                    break;
+                case 1:
+                    result = SearchResult.builder().withPosition(1).withFound(true).build();
+                    break;
+                case 16:
+                    result = SearchResult.builder().withPosition(4).withFound(true).build();
+                    break;
+            }
+            return result;
+        });
+        int seq1[] = { -15, -8, 0, 1, 11, 16 };
+        int seq2[] = { -15, 1, 8, 9, 16 };
+        double res = finder.calculateJackardSimilarity(seq1, seq2);
+        assertEquals(3d / (seq1.length + seq2.length - 3), res);
     }
 
 }
