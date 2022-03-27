8c43a89ebff9ce61b5f71aab41c21b7ccc4275fb
SzymonGoldberg <szymon.gorka.sgorkas@gmail.com>
2022-03-27 22:28:49 +0200

    Added state tests.

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -1,14 +1,56 @@
 package edu.iis.mto.similarity;
 
-import static org.junit.jupiter.api.Assertions.fail;
-
+import edu.iis.mto.searcher.SearchResult;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class SimilarityFinderTest {
 
-    @Test
-    void test() {
-        fail("Not yet implemented");
+    SimilarityFinder finder;
+    @BeforeEach
+    void initSearch() {
+        finder = new SimilarityFinder(((elem, sequence) -> SearchResult.builder().build()));
     }
 
+    @Test
+    void testSameNonEmptySequences() {
+        int[] seq1 = {0, 1, 2};
+        int[] seq2 = {0, 1, 2};
+        finder = new SimilarityFinder(((elem, sequence) -> {
+            switch (elem) {
+                case 0:
+                    return SearchResult.builder().withPosition(0).withFound(true).build();
+                case 1:
+                    return SearchResult.builder().withPosition(1).withFound(true).build();
+                case 2:
+                    return SearchResult.builder().withPosition(2).withFound(true).build();
+                default:
+                    return null;
+            }
+        }));
+        assertEquals(1.0, finder.calculateJackardSimilarity(seq1, seq2));
+    }
+
+    @Test
+    void testEmptySequences() {
+        int[] seq1 = {};
+        int[] seq2 = {};
+        assertEquals(1.0, finder.calculateJackardSimilarity(seq1, seq2));
+    }
+
+    @Test
+    void testFirstEmptySequence() {
+        int[] seq1 = {};
+        int[] seq2 = {0, 1, 2};
+        assertEquals(0.0, finder.calculateJackardSimilarity(seq1, seq2));
+    }
+
+    @Test
+    void testSecondEmptySequence() {
+        int[] seq1 = {0, 1, 2};
+        int[] seq2 = {};
+        assertEquals(0.0, finder.calculateJackardSimilarity(seq1, seq2));
+    }
 }
