70832334b9fccba60d16fac0006cd1c2ea1e3d24
MateuszMatusiak <mateusz.matusiak000@gmail.com>
2022-03-30 10:57:51 +0200

    puste sekwencje

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -1,14 +1,21 @@
 package edu.iis.mto.similarity;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
+import edu.iis.mto.searcher.SearchResult;
 import org.junit.jupiter.api.Test;
 
 class SimilarityFinderTest {
 
     @Test
-    void test() {
-        fail("Not yet implemented");
+    void emptySequences(){
+        int[] seq1 = new int[0];
+        int[] seq2 = new int[0];
+
+        SimilarityFinder similarityFinder = new SimilarityFinder((elem,seq)->SearchResult.builder().build());
+        double res = similarityFinder.calculateJackardSimilarity(seq1,seq2);
+        assertEquals(1,res);
     }
 
 }
