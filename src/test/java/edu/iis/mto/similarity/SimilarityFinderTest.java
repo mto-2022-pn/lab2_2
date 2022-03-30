103127548cdb0145270b6b24481c8a8a4c5c46cc
Adrian Pietrzak <78472697+AdrianPietrz@users.noreply.github.com>
2022-03-30 20:40:20 +0200

    przeniesienie testow do jednej klasy oraz nadanie odpowiednich nazw

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -9,6 +9,8 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Field;
+
 class SimilarityFinderTest {
 
     SimilarityFinder similarityFinder;
@@ -18,7 +20,7 @@ class SimilarityFinderTest {
     }
 
     @Test
-    void SentencesAreEmpty() {
+    void sentencesAreEmptyMethodShouldReturnFullSimilarity() {
         int seq [] = new int[0];
         int seq1 [] = new int[0];
         double result = similarityFinder.calculateJackardSimilarity(seq,seq1);
@@ -26,7 +28,7 @@ class SimilarityFinderTest {
     }
 
     @Test
-    void OneSentenceIsEmpty() {
+    void oneSentenceIsEmptyMethodShouldReturnNoSimilarity() {
         int seq [] = new int[]{1,2,3};
         int seq1 [] = new int[0];
         double result = similarityFinder.calculateJackardSimilarity(seq,seq1);
@@ -36,7 +38,7 @@ class SimilarityFinderTest {
     }
 
     @Test
-    void sequencesAreTheSame()
+    void sequencesAreTheSameMethodShouldReturnFullSimilarity()
     {
         int[] seq = {1, 2, 3, 4};
         int[] seq1 = {1, 2, 3, 4};
@@ -58,7 +60,7 @@ class SimilarityFinderTest {
     }
 
     @Test
-    void sequencesHaveCommonValues()
+    void sequencesHaveCommonValuesMethodShouldReturnSimilarityInRange0to1()
     {
         int[] seq = {1, 2, 3, 4};
         int[] seq1 = {3, 4, 5, 6,7,8,9,10};
@@ -77,6 +79,56 @@ class SimilarityFinderTest {
         });
         double result = similarityFinder.calculateJackardSimilarity(seq, seq1);
         assertEquals(2.0/(seq.length+seq1.length-2),result);
+    }
+
+
+    @Test
+    void searchMethodShouldBeCalledTwice() throws NoSuchFieldException, IllegalAccessException {
+
+        int[] seq = {1, 2};
+        int[] seq1 = {1, 2, 3, 4};
+
+        SequenceSearcher sequenceSearcher = new SequenceSearcher() {
+            int count = 0;
+            @Override
+            public SearchResult search(int elem, int[] sequence) {
+                SearchResult searchResult = null;
+                count++;
+                if (elem == 1)
+                    searchResult = SearchResult.builder().withPosition(0).withFound(false).build();
+                if (elem == 2)
+                    searchResult = SearchResult.builder().withPosition(1).withFound(false).build();
+                return searchResult;
+            }
+        };
+        SimilarityFinder similarityFinder = new SimilarityFinder(sequenceSearcher);
+        similarityFinder.calculateJackardSimilarity(seq,seq1);
+        Field check = sequenceSearcher.getClass().getDeclaredField("count");
+        int checkInt = check.getInt(sequenceSearcher);
+        assertEquals(2,checkInt);
+    }
+
+    @Test
+    void invokesCountTestWithEmptyArrayMethodShouldNotBeCalled() throws NoSuchFieldException, IllegalAccessException {
+
+        int[] seq = {};
+        int[] seq1 = {1, 2, 3, 4};
+
+        SequenceSearcher sequenceSearcher = new SequenceSearcher() {
+            int count = 0;
+            @Override
+            public SearchResult search(int elem, int[] sequence) {
+                SearchResult searchResult = null;
+                count++;
+                searchResult = SearchResult.builder().build();
+                return searchResult;
+            }
+        };
+        SimilarityFinder similarityFinder = new SimilarityFinder(sequenceSearcher);
+        similarityFinder.calculateJackardSimilarity(seq,seq1);
+        Field check = sequenceSearcher.getClass().getDeclaredField("count");
+        int checkInt = check.getInt(sequenceSearcher);
+        assertEquals(0,checkInt);
     }
 
 }
