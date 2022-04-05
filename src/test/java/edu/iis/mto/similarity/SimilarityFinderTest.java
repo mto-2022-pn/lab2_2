5e27f410e97a27eb0f7b132ba0657be48af1311a
Norbert Łodziński <209382@edu.p.lodz.pl>
2022-04-05 09:18:58 +0200

    Added tests for three elem sequences (same, different, similar)

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -45,4 +45,35 @@ class SimilarityFinderTest {
         assertEquals(0.0d, result);
     }
 
+    @Test
+    void sameThreeElemSequences_shouldReturnOne() {
+        int[] seq1 = {11,220,5};
+        int[] seq2 = {11,220,5};
+
+        double result = simFinder.calculateJackardSimilarity(seq1, seq2);
+
+        assertEquals(1.0d, result);
+    }
+
+    @Test
+    void differentThreeElemSequences_shouldReturnZero() {
+        int[] seq1 = {1,22,333};
+        int[] seq2 = {4444,55555,666666};
+
+        double result = simFinder.calculateJackardSimilarity(seq1, seq2);
+
+        assertEquals(0.0d, result);
+    }
+
+    @Test
+    void oneElemSameInThreeElemSequences_shouldReturnPointTwo() {
+        int[] seq1 = {100,20,300};
+        int[] seq2 = {400,100,50};
+
+        // 1 / (3 + 3 - 1) = 0.2
+        double result = simFinder.calculateJackardSimilarity(seq1, seq2);
+
+        assertEquals(0.2d, result);
+    }
+
 }
