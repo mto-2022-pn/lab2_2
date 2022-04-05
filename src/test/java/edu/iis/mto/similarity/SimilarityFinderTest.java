40009df2dfbd19e8d3ce13645bf946a25031b216
Norbert Łodziński <209382@edu.p.lodz.pl>
2022-04-05 09:13:46 +0200

    Added state tests for:  - zero elem sequences  - one elem sequences (same and different)

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -1,14 +1,48 @@
 package edu.iis.mto.similarity;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class SimilarityFinderTest {
+    SimilarityFinder simFinder;
+
+    @BeforeEach
+    void setup() {
+        simFinder = new SimilarityFinder(new FakeSequenceSearcher());
+    }
 
     @Test
-    void test() {
-        fail("Not yet implemented");
+    void zeroElemSequences_shouldReturnOne() {
+        int[] seq1 = new int[0];
+        int[] seq2 = new int[0];
+
+        double result = simFinder.calculateJackardSimilarity(seq1, seq2);
+
+        assertEquals(1.0d, result);
+    }
+
+    @Test
+    void sameOneElemSequences_shouldReturnOne() {
+        int[] seq1 = {1};
+        int[] seq2 = {1};
+
+        //1 / (1+1-1)
+        double result = simFinder.calculateJackardSimilarity(seq1, seq2);
+
+        assertEquals(1.0d, result);
+    }
+
+    @Test
+    void differentOneElemSequences_shouldReturnZero() {
+        int[] seq1 = {1};
+        int[] seq2 = {0};
+
+        double result = simFinder.calculateJackardSimilarity(seq1, seq2);
+
+        assertEquals(0.0d, result);
     }
 
 }
