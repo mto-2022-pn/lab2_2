a78b0322adddfd3e73b3200b6a1703fb81ae5a59
Norbert Łodziński <209382@edu.p.lodz.pl>
2022-04-05 09:56:53 +0200

    Added behavioral tests for:  - zero elem sequence (search should not be invoked)  - different length sequences (search should invoke n times, where n = seq1.length)

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -108,4 +108,52 @@ class SimilarityFinderTest {
 
         assertEquals(4.0d / 6.0d, result);
     }
+
+    @Test
+    void zeroElemSequences_searchMethodShouldNotBeInvoked() {
+        int[] seq1 = new int[0];
+        int[] seq2 = new int[0];
+
+        MockSequenceSearcher mock = new MockSequenceSearcher();
+        simFinder = new SimilarityFinder(mock);
+        simFinder.calculateJackardSimilarity(seq1, seq2);
+
+        assertEquals(0, mock.getSearchInvokeCounter());
+    }
+
+    @Test
+    void oneElemSequences_searchShouldBeInvokedOnce() {
+        int[] seq1 = {123};
+        int[] seq2 = {205};
+
+        MockSequenceSearcher mock = new MockSequenceSearcher();
+        simFinder = new SimilarityFinder(mock);
+        simFinder.calculateJackardSimilarity(seq1, seq2);
+
+        assertEquals(1, mock.getSearchInvokeCounter());
+    }
+
+    @Test
+    void fiveAndEightElemSequences_searchShouldBeInvokedSixTimes() {
+        int[] seq1 = {123, 1, 99, 1245, 5652};
+        int[] seq2 = {205, 146, 532, 556, 1562, 12, 146, 77};
+
+        MockSequenceSearcher mock = new MockSequenceSearcher();
+        simFinder = new SimilarityFinder(mock);
+        simFinder.calculateJackardSimilarity(seq1, seq2);
+
+        assertEquals(5, mock.getSearchInvokeCounter());
+    }
+
+    @Test
+    void sevenAndFourElemSequences_searchShouldBeInvokedEightTimes() {
+        int[] seq1 = {221, 142, 2, 56, 12, 1, 14};
+        int[] seq2 = {123, 1, 99, 2, 22};
+
+        MockSequenceSearcher mock = new MockSequenceSearcher();
+        simFinder = new SimilarityFinder(mock);
+        simFinder.calculateJackardSimilarity(seq1, seq2);
+
+        assertEquals(7, mock.getSearchInvokeCounter());
+    }
 }
