67e88e6b0b19897c6657853cca704f068e76c744
JakZaj <jakubzajac13@gmail.com>
2022-04-09 11:20:45 +0200

    Added tests difftent sequences, some same elements and corrected non empty same sequences

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -37,9 +37,9 @@ class SimilarityFinderTest {
                case 3:
                    return SearchResult.builder().withFound(true).withPosition(2).build();
                case 4:
-                   return SearchResult.builder().withFound(true).withPosition(4).build();
+                   return SearchResult.builder().withFound(true).withPosition(3).build();
            }
-           return null;
+           return SearchResult.builder().build();
         });
 
         assertEquals(1.0, similarityFinder.calculateJackardSimilarity(seq1, seq2));
@@ -57,6 +57,30 @@ class SimilarityFinderTest {
         int[] seq1 = {1, 2, 3, 4};
         int[] seq2 = {};
         assertEquals(0.0, similarityFinder.calculateJackardSimilarity(seq1, seq2));
+    }
+
+    @Test
+    void testDiffrentSequences() {
+        int[] seq1 = {1, 2, 3, 4};
+        int[] seq2 = {5, 6, 7, 8};
+        assertEquals(0.0, similarityFinder.calculateJackardSimilarity(seq1, seq2));
+    }
+
+    @Test
+    void testSomeSameElements() {
+        int[] seq1 = {1, 2, 3, 4, 7};
+        int[] seq2 = {3, 4, 5, 6, 8};
+        similarityFinder=new SimilarityFinder((elem, sequence) -> {
+            switch (elem){
+                case 3:
+                    return SearchResult.builder().withFound(true).withPosition(0).build();
+                case 4:
+                    return SearchResult.builder().withFound(true).withPosition(1).build();
+            }
+            return SearchResult.builder().build();
+        });
+
+        assertEquals(2.0/(seq1.length + seq2.length - 2), similarityFinder.calculateJackardSimilarity(seq1, seq2));
     }
 
 }
