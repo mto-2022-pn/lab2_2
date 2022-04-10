4df491122d9c6bc28adbc1c92d53452d8d142d76
Hubert Krauza <hubertkrauza@gmail.com>
2022-04-11 01:52:24 +0200

    empty sequences tests

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -1,14 +1,43 @@
 package edu.iis.mto.similarity;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
+import edu.iis.mto.searcher.SearchResult;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class SimilarityFinderTest {
 
+    private SimilarityFinder finder;
+
+    @BeforeEach
+    void setUp() {
+        finder = new SimilarityFinder((elem, seq) -> SearchResult.builder().build());
+    }
+
     @Test
-    void test() {
-        fail("Not yet implemented");
+    void testBothSequencesEmpty() {
+        int[] seq1 = {};
+        int[] seq2 = {};
+        double result = finder.calculateJackardSimilarity(seq1, seq2);
+        assertEquals(1.0d, result);
+    }
+
+    @Test
+    void testFirstSequenceEmpty() {
+        int[] seq1 = {};
+        int[] seq2 = {1,2,3};
+        double result = finder.calculateJackardSimilarity(seq1, seq2);
+        assertEquals(0.0d, result);
+    }
+
+    @Test
+    void testSecondSequenceEmpty() {
+        int[] seq1 = {};
+        int[] seq2 = {1,2,3};
+        double result = finder.calculateJackardSimilarity(seq1, seq2);
+        assertEquals(0.0d, result);
     }
 
 }
