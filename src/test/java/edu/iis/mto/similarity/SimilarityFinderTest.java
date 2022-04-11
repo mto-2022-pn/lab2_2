2e67be3f4a94dd61c50fab5c2525f78517b1b99c
Hubert Krauza <hubertkrauza@gmail.com>
2022-04-11 02:10:12 +0200

    sequences equals test

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -11,13 +11,9 @@ class SimilarityFinderTest {
 
     private SimilarityFinder finder;
 
-    @BeforeEach
-    void setUp() {
-        finder = new SimilarityFinder((elem, seq) -> SearchResult.builder().build());
-    }
-
     @Test
     void testBothSequencesEmpty() {
+        finder = new SimilarityFinder((elem, seq) -> SearchResult.builder().build());
         int[] seq1 = {};
         int[] seq2 = {};
         double result = finder.calculateJackardSimilarity(seq1, seq2);
@@ -26,6 +22,7 @@ class SimilarityFinderTest {
 
     @Test
     void testFirstSequenceEmpty() {
+        finder = new SimilarityFinder((elem, seq) -> SearchResult.builder().build());
         int[] seq1 = {};
         int[] seq2 = {1,2,3};
         double result = finder.calculateJackardSimilarity(seq1, seq2);
@@ -34,10 +31,32 @@ class SimilarityFinderTest {
 
     @Test
     void testSecondSequenceEmpty() {
+        finder = new SimilarityFinder((elem, seq) -> SearchResult.builder().build());
         int[] seq1 = {};
         int[] seq2 = {1,2,3};
         double result = finder.calculateJackardSimilarity(seq1, seq2);
         assertEquals(0.0d, result);
+    }
+
+    @Test
+    void testSequencesEquals() {
+        finder = new SimilarityFinder((elem, seq) -> {
+            SearchResult result;
+            switch(elem){
+                case 1:
+                case 2:
+                case 3:
+                    result = SearchResult.builder().withFound(true).build();
+                    break;
+                default:
+                    result = SearchResult.builder().withFound(false).build();
+            }
+            return result;
+        });
+        int[] seq1 = {1,2,3};
+        int[] seq2 = {1,2,3};
+        double result = finder.calculateJackardSimilarity(seq1, seq2);
+        assertEquals(1.0d, result);
     }
 
 }
