495eb5f8e53e74e16a0be557302f2583ac95c25c
Karol Profic <karolprofic@protonmail.com>
2022-04-11 17:33:08 +0200

    Dodanie zarysu testow

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -7,8 +7,34 @@ import org.junit.jupiter.api.Test;
 class SimilarityFinderTest {
 
     @Test
-    void test() {
+    void bothArrayEmpty() {
         fail("Not yet implemented");
     }
+
+    @Test
+    void firstArrayEmpty() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void secondArrayEmpty() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void arraysDontHaveAnyElementsInCommon() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void arraysHaveSomeCommonElements() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void arraysAreIdenticals() {
+        fail("Not yet implemented");
+    }
+
 
 }
