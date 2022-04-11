05c4c29562ea990c0a65bdb0bbdcd89cac49557f
Karol Profic <karolprofic@protonmail.com>
2022-04-11 17:34:55 +0200

    Dodanie testu bothArrayEmpty

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -1,14 +1,19 @@
 package edu.iis.mto.similarity;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
+import edu.iis.mto.searcher.SearchResult;
 import org.junit.jupiter.api.Test;
 
 class SimilarityFinderTest {
 
     @Test
     void bothArrayEmpty() {
-        fail("Not yet implemented");
+        int[] seq1 = {};
+        int[] seq2 = {};
+        SimilarityFinder finder = new SimilarityFinder((element, sequence) -> SearchResult.builder().build());
+        assertEquals(1.0d, finder.calculateJackardSimilarity(seq1, seq2));
     }
 
     @Test
