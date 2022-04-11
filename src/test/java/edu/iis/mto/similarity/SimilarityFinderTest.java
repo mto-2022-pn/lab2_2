0823391cafaac5dc0f5e4a537abf56d8739926c9
Karol Profic <karolprofic@protonmail.com>
2022-04-11 17:53:19 +0200

    Test obie tablice takie same i podobne w połowie

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -42,12 +42,55 @@ class SimilarityFinderTest {
 
     @Test
     void arraysHaveSomeCommonElements() {
-        fail("Not yet implemented");
+        SimilarityFinder finder = new SimilarityFinder((element, sequence) -> {
+            SearchResult result = SearchResult.builder().build();
+            switch (element) {
+                case 1:
+                    result = SearchResult.builder().withPosition(0).withFound(true).build();
+                    break;
+                case 2:
+                    result = SearchResult.builder().withPosition(1).withFound(false).build();
+                    break;
+                case 3:
+                    result = SearchResult.builder().withPosition(2).withFound(true).build();
+                    break;
+                case 4:
+                    result = SearchResult.builder().withPosition(3).withFound(true).build();
+                    break;
+                case 5:
+                    result = SearchResult.builder().withPosition(4).withFound(true).build();
+                    break;
+                case 6:
+                    result = SearchResult.builder().withPosition(5).withFound(false).build();
+                    break;
+            }
+            return result;
+        });
+        int[] seq1 = {1, 2, 3, 4, 5, 6};
+        int[] seq2 = {1, 4, 3, 9, 5, 7};
+        assertEquals(0.5d, finder.calculateJackardSimilarity(seq1, seq2));
     }
 
     @Test
-    void arraysAreIdenticals() {
-        fail("Not yet implemented");
+    void arraysAreIdentical() {
+        SimilarityFinder finder = new SimilarityFinder((element, sequence) -> {
+            SearchResult result = SearchResult.builder().build();
+            switch (element) {
+                case 1:
+                    result = SearchResult.builder().withPosition(0).withFound(true).build();
+                    break;
+                case 2:
+                    result = SearchResult.builder().withPosition(1).withFound(true).build();
+                    break;
+                case 3:
+                    result = SearchResult.builder().withPosition(2).withFound(true).build();
+                    break;
+            }
+            return result;
+        });
+        int[] seq1 = {1, 2, 3};
+        int[] seq2 = {1, 2, 3};
+        assertEquals(1.0d, finder.calculateJackardSimilarity(seq1, seq2));
     }
 
 
