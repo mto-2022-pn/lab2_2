b5e7933af2df9ed64d2cb1bd6a14b8813f872133
Hubert Krauza <hubertkrauza@gmail.com>
2022-04-11 02:42:25 +0200

    sequences different, common and includes - tests

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class SimilarityFinderTest {
-
+// zbiór sklada sie z niepowtarzajacych elementow
     private SimilarityFinder finder;
 
     @Test
@@ -59,4 +59,87 @@ class SimilarityFinderTest {
         assertEquals(1.0d, result);
     }
 
+    @Test
+    void testSequencesTotallyDifferent() {
+        finder = new SimilarityFinder((elem, seq) -> {
+            SearchResult result;
+            switch(elem){
+                case 1:
+                case 3:
+                case 5:
+                    result = SearchResult.builder().withFound(false).build();
+                    break;
+                default:
+                    result = SearchResult.builder().withFound(true).build();
+            }
+            return result;
+        });
+        int[] seq1 = {1,3,5};
+        int[] seq2 = {2,4,6,8};
+        double result = finder.calculateJackardSimilarity(seq1, seq2);
+        assertEquals(0.0d, result);
+    }
+
+    @Test
+    void testSequencesWithSomeCommonElements() {
+        finder = new SimilarityFinder((elem, seq) -> {
+            SearchResult result;
+            switch(elem){
+                case 1:
+                case 5:
+                case 7:
+                    result = SearchResult.builder().withFound(true).build();
+                    break;
+                default:
+                    result = SearchResult.builder().withFound(false).build();
+            }
+            return result;
+        });
+        int[] seq1 = {1,2,3,5,7};
+        int[] seq2 = {1,4,5,6,7,8};
+        double result = finder.calculateJackardSimilarity(seq1, seq2);
+        assertEquals(3.0d / (seq1.length + seq2.length - 3), result);
+    }
+
+    @Test
+    void testFirstSequenceInSecondSequence() {
+        finder = new SimilarityFinder((elem, seq) -> {
+            SearchResult result;
+            switch(elem){
+                case 1:
+                case 5:
+                case 7:
+                    result = SearchResult.builder().withFound(true).build();
+                    break;
+                default:
+                    result = SearchResult.builder().withFound(false).build();
+            }
+            return result;
+        });
+        int[] seq1 = {1,5,7};
+        int[] seq2 = {1,4,5,6,7,8};
+        double result = finder.calculateJackardSimilarity(seq1, seq2);
+        assertEquals(3.0d / (seq1.length + seq2.length - 3), result);
+    }
+
+    @Test
+    void testSecondSequenceInFirstSequence() {
+        finder = new SimilarityFinder((elem, seq) -> {
+            SearchResult result;
+            switch(elem){
+                case 1:
+                case 5:
+                case 7:
+                    result = SearchResult.builder().withFound(true).build();
+                    break;
+                default:
+                    result = SearchResult.builder().withFound(false).build();
+            }
+            return result;
+        });
+        int[] seq1 = {1,2,3,5,7};
+        int[] seq2 = {1,5,7};
+        double result = finder.calculateJackardSimilarity(seq1, seq2);
+        assertEquals(3.0d / (seq1.length + seq2.length - 3), result);
+    }
 }
